0484d1754d41741ecca8f8be8510c5ed670d21cb
ehewitt <ehewitt@picodigital.com>
2018-09-05 17:03:00 -0700

    Updates_180905

--- Paycheck.sh
+++ Paycheck.sh
@@ -17,8 +17,8 @@ echo Mortgage,738,$(date +%y%m%d),Payday,-738,Transfer | tee -a Mortgage.txt Tot
 echo Payday,-738,$(date +%y%m%d),Mortgage,738,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Car,73,$(date +%y%m%d),Payday,-73,Transfer | tee -a Car.txt Total.txt >/dev/null
 echo Payday,-73,$(date +%y%m%d),Car,73,Transfer | tee -a Payday.txt Total.txt >/dev/null
-echo Gas,140,$(date +%y%m%d),Payday,-140,Transfer | tee -a Gas.txt Total.txt >/dev/null
-echo Payday,-140,$(date +%y%m%d),Gas,140,Transfer | tee -a Payday.txt Total.txt >/dev/null
+echo Gas,130,$(date +%y%m%d),Payday,-130,Transfer | tee -a Gas.txt Total.txt >/dev/null
+echo Payday,-130,$(date +%y%m%d),Gas,130,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Phone,18,$(date +%y%m%d),Payday,-18,Transfer | tee -a Phone.txt Total.txt >/dev/null
 echo Payday,-18,$(date +%y%m%d),Phone,18,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Live,197,$(date +%y%m%d),Payday,-197,Transfer | tee -a Live.txt Total.txt >/dev/null
@@ -35,8 +35,8 @@ echo CreditCard,30,$(date +%y%m%d),Payday,-30,Transfer | tee -a CreditCard.txt T
 echo Payday,-30,$(date +%y%m%d),CreditCard,30,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo HOA,135,$(date +%y%m%d),Payday,-135,Transfer | tee -a HOA.txt Total.txt >/dev/null
 echo Payday,-135,$(date +%y%m%d),HOA,135,Transfer | tee -a Payday.txt Total.txt >/dev/null
-echo SDGE,40,$(date +%y%m%d),Payday,-40,Transfer | tee -a SDGE.txt Total.txt >/dev/null
-echo Payday,-40,$(date +%y%m%d),SDGE,40,Transfer | tee -a Payday.txt Total.txt >/dev/null
+echo SDGE,50,$(date +%y%m%d),Payday,-50,Transfer | tee -a SDGE.txt Total.txt >/dev/null
+echo Payday,-50,$(date +%y%m%d),SDGE,50,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Debts,60,$(date +%y%m%d),Payday,-60,Transfer | tee -a Debts.txt Total.txt >/dev/null
 echo Payday,-60,$(date +%y%m%d),Debts,60,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Invest,60,$(date +%y%m%d),Payday,-60,Transfer | tee -a Invest.txt Total.txt >/dev/null

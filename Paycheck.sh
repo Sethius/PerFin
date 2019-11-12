804aa6ff4c1003a8ea684fc38d2c097b5c5b1ce9
ehewitt <ehewitt@picodigital.com>
2019-11-12 10:38:01 -0800

    Updates_191112

--- Paycheck.sh
+++ Paycheck.sh
@@ -17,8 +17,8 @@ echo AAA,50,$(date +%y%m%d),Payday,-50,Transfer | tee -a AAA.txt Total.txt >/dev
 echo Payday,-50,$(date +%y%m%d),AAA,50,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Car,95,$(date +%y%m%d),Payday,-95,Transfer | tee -a Car.txt Total.txt >/dev/null
 echo Payday,-95,$(date +%y%m%d),Car,95,Transfer | tee -a Payday.txt Total.txt >/dev/null
-echo Debts,130,$(date +%y%m%d),Payday,-130,Transfer | tee -a Debts.txt Total.txt >/dev/null
-echo Payday,-130,$(date +%y%m%d),Debts,130,Transfer | tee -a Payday.txt Total.txt >/dev/null
+echo Debts,84,$(date +%y%m%d),Payday,-84,Transfer | tee -a Debts.txt Total.txt >/dev/null
+echo Payday,-84,$(date +%y%m%d),Debts,84,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Emergency,206,$(date +%y%m%d),Payday,-206,Transfer | tee -a Emergency.txt Total.txt >/dev/null
 echo Payday,-206,$(date +%y%m%d),Emergency,206,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Extras,130,$(date +%y%m%d),Payday,-130,Transfer | tee -a Extras.txt Total.txt >/dev/null
@@ -37,6 +37,8 @@ echo Live,149,$(date +%y%m%d),Payday,-149,Transfer | tee -a Live.txt Total.txt >
 echo Payday,-149,$(date +%y%m%d),Live,149,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Mortgage,738,$(date +%y%m%d),Payday,-738,Transfer | tee -a Mortgage.txt Total.txt >/dev/null
 echo Payday,-738,$(date +%y%m%d),Mortgage,738,Transfer | tee -a Payday.txt Total.txt >/dev/null
+echo MunkyFred,46,$(date +%y%m%d),Payday,-46,Transfer | tee -a MunkyFred.txt Total.txt >/dev/null
+echo Payday,-46,$(date +%y%m%d),MunkyFred,46,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo Phone,125,$(date +%y%m%d),Payday,-125,Transfer | tee -a Phone.txt Total.txt >/dev/null
 echo Payday,-125,$(date +%y%m%d),Phone,125,Transfer | tee -a Payday.txt Total.txt >/dev/null
 echo SDGE,50,$(date +%y%m%d),Payday,-50,Transfer | tee -a SDGE.txt Total.txt >/dev/null

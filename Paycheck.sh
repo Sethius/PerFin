f09002996afb2d5e7cce69d05f10648bc42bf3da
ehewitt <ehewitt@picodigital.com>
2020-05-11 11:29:29 -0700

    Updates_200511

--- Paycheck.sh
+++ Paycheck.sh
@@ -13,21 +13,20 @@ read paycheck
 ##Some of these will need to be updated periodically to reflect changes in priority
 
 ./Input.sh AAA,50,$(date +%y%m%d),Payday,-50,Transfer
-./Input.sh Car,100,$(date +%y%m%d),Payday,-100,Transfer
-./Input.sh Debts,89,$(date +%y%m%d),Payday,-89,Transfer
-./Input.sh Emergency,210,$(date +%y%m%d),Payday,-210,Transfer
-./Input.sh Extras,135,$(date +%y%m%d),Payday,-135,Transfer
+./Input.sh Car,171,$(date +%y%m%d),Payday,-171,Transfer
+./Input.sh Debts,-23,$(date +%y%m%d),Payday,23,Transfer
+./Input.sh Emergency,281,$(date +%y%m%d),Payday,-281,Transfer
+./Input.sh Extras,206,$(date +%y%m%d),Payday,-206,Transfer
 ./Input.sh Gaming,25,$(date +%y%m%d),Payday,-25,Transfer
 ./Input.sh Gas,120,$(date +%y%m%d),Payday,-120,Transfer
 ./Input.sh HOA,150,$(date +%y%m%d),Payday,-150,Transfer
 ./Input.sh Internet,40,$(date +%y%m%d),Payday,-40,Transfer
-./Input.sh Invest,135,$(date +%y%m%d),Payday,-135,Transfer
-./Input.sh Live,154,$(date +%y%m%d),Payday,-154,Transfer
-./Input.sh Mortgage,738,$(date +%y%m%d),Payday,-738,Transfer
+./Input.sh Invest,206,$(date +%y%m%d),Payday,-206,Transfer
+./Input.sh Live,225,$(date +%y%m%d),Payday,-225,Transfer
+./Input.sh Mortgage,568,$(date +%y%m%d),Payday,-568,Transfer
 ./Input.sh MunkyFred,46,$(date +%y%m%d),Payday,-46,Transfer
 ./Input.sh Phone,95,$(date +%y%m%d),Payday,-95,Transfer
 ./Input.sh SDGE,50,$(date +%y%m%d),Payday,-50,Transfer
-./Input.sh Taxes,58,$(date +%y%m%d),Payday,-58,Transfer
 
 echo
 
